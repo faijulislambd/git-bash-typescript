2077e8580d794350d4046fd07ebd0b06abb90ebd
faijulislambd <f.faijulislam144@gmail.com>
2024-11-21 22:20:27 +0600

    Updated sshAgentAdd script to push files in git after adding agent

--- Bash/sshAgentAdd.sh
+++ Bash/sshAgentAdd.sh
@@ -9,8 +9,13 @@ read -p "Enter SSH Key Name: " sshKey
 # Check if name entered
 if [ -z ${sshKey} ]; then
     echo "No Key Name Entered"
+    exit 1
 else
     ssh-add ~/.ssh/$sshKey
     ssh-add -l
-
+    # Check if git connection successfull
+    ssh -T git@github.com
+    # Push after agent is sure
+    git push origin master
+    exit 0
 fi
